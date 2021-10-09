560813493df02e71ec000ed397afb245aa3a6a86
Nik212 <gevnik08@gmail.com>
2021-10-09 18:30:47 +0300

    asdbsdfgd

--- useful_package/__init__.py
+++ useful_package/__init__.py
@@ -1,6 +1,8 @@
 from module_a import polynom_3
-
+from module_b import hyperbola
 
 if __name__ == '__main__':
     x = 3
     print(polynom_3(x))
+
+    print(hyperbola(x))
---
+++ useful_package/module_b.py
@@ -0,0 +1,4 @@
+def hyperbola(x):
+    if (x == 0):
+        return 0
+    return 1/x
